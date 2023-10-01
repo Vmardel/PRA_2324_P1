19ed364b9faafac59eac5ae156692487645a1ba5
Vicent Martinez de la Concepción <vmardel@epsa.edu.es>
2023-10-01 12:48:54 +0200

    Añadido correccion list.h y implementacion listarray.h pendiente de correcciones futuras

--- ListArray.h
+++ ListArray.h
@@ -1,6 +1,7 @@
 #include <ostream>
 #include "List.h"
 #include <iostream>
+
 template <typename T> 
 class ListArray : public List<T> {
 
@@ -8,49 +9,56 @@ class ListArray : public List<T> {
 	T *arr;
 	int max;
 	int a;
-	static const int MINSIZE;
+	static const int MINSIZE = 2;
 	void resize(int new_size){
-
+		T arr2[new_size];
+		for( int i = 0; i < new_size; i++){
+			arr2[i] = arr[i];
+		}
+		delete arr;
+		arr -> arr2;
+		max = new_size;	
 	}	
         // miembros privados
 
     public:
-	virtual void insert(int pos, T e){
+	void insert(int pos, T e){
 		if(e > 0 && e < size()){
 			arr[pos] = e;
 		}
-		else(){
+		else{
 			std::cout << "fuera de rango" << std::endl;
 		}
 	}
 
-	virtual void append(T e){
+	void append(T e){
 		arr[max] = e;
 	}
 
-	virtual void prepend(T e){
+	void prepend(T e){
 		arr[0] = e;
 	}
 
-	virtual T remove(int pos){
-		if(e >= 0 && e < size()-1){
+	T remove(int pos){
+		if(pos >= 0 && pos < size()-1){
                         return arr[pos];
 			delete[] arr[pos];
                 }
-                else(){
+                else{
                         std::cout << "fuera de rango" << std::endl;
                 }
 	}
-	virtual T get(int pos) const{
-		if(e >= 0 && e < size()-1){
+	
+	T get(int pos) const{
+		if(pos >= 0 && pos < size()-1){
                         return arr[pos];
                 }
-                else(){
+                else{
                         std::cout << "fuera de rango" << std::endl;
                 }
         }
 
-	virtual int search(T e) const{
+	int search(T e) const{
 		int i = 0;
 		for( i; i < size(); i++){
 			if( arr[i] == e){
@@ -60,16 +68,17 @@ class ListArray : public List<T> {
 		return -1;
 	}
 
-	virtual bool empty() const{
+	bool empty() const{
 		for(int i = 0; i < size(); i++){
 			if( arr[i] != nullptr){
 				return true;
+				break;
 			}
 		}
 		return false;
 	}
 
-	virtual int size() const{
+	int size() const{
 		for(int i = 0; i < i++; i++){
 			if(arr[i] == nullptr){
 				return i;
@@ -77,13 +86,30 @@ class ListArray : public List<T> {
 		}
 	}
 
-	ListArray();
-	~ListArray();
-	T operator[](int pos);
-	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+	ListArray(){
+		if( MINSIZE != 2 ){
+			throw std::invalid_argument("valor erroneo en el MINSIZE" );
+		}
+		*arr = new T[MINSIZE];
 	}
 
-	
-        // miembros públicos, incluidos los heredados de List<T>
-    
+	~ListArray(){
+		delete[] *arr;
+	}
+
+	T operator[](int pos){
+		if( pos < 0 || pos > size-1 ){
+			throw std::out_of_range("esta fuera de rango" );
+		}
+		else{
+			return arr[pos];		
+		}
+	}	
+
+	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+			out << ListArray();
+			return out;
+	}
+
+        // miembros públicos, incluidos los heredados de List<T>   
 };
